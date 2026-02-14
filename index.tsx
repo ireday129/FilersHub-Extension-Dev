8e5f608196619763de7f4ca8cbfef921233bb2ef
Irene Day <contact@hannahfinancial.com>
2026-02-14 01:36:40 -0500

    chore: update frontend configuration and styles

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
 // Prevent duplicate root creation during hot-reloads in the editor environment
